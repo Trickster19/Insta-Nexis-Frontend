f9c5034e7bbff85cf6a1e4d22f57be646674b2ad
Satwik Agrawal <satwikcoolagrawal@gmail.com>
2024-11-16 18:21:35 +0530

    hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import { Button } from './components/ui/button'
-import { useStore } from './store/store'
+import "./App.css";
+// import { Button } from "./components/ui/button";
+import HeroSection from "./sections/HeroSection";
+// import { useStore } from "./store/store";
 
-interface CountState {
-  count: number,
-  increase: () => void
-}
+// interface CountState {
+//   count: number;
+//   increase: () => void;
+// }
 function App() {
-  const count = useStore((state: CountState) => state.count);
-  const increase = useStore((state: CountState) => state.increase);
-  return (
-    <div style={{ display: 'flex', 
-       justifyContent: 'center',
-       alignItems: 'center',
-       height: '100vh'
-    }}>
-    <Button onClick={increase}>
-      Count {count}
-    </Button>
-    </div>
-  )
+  // const count = useStore((state: CountState) => state.count);
+  // const increase = useStore((state: CountState) => state.increase);
+  return <HeroSection />;
 }
 
-export default App
+export default App;
