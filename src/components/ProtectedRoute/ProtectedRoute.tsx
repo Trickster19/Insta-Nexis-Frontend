a4b11af120d2332faf06c5633fa90c057849dcbc
Trickster19 <harrismohd786@gmail.com>
2024-11-17 15:01:20 +0530

    Protected component update and Start With us Button handling

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,18 +1,30 @@
 import useAuth from "@/store";
-import React, { useEffect } from "react";
-import { Navigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, useNavigate } from "react-router-dom";
 
 interface Props {
   children: React.ReactNode;
   authenticationRequired: Boolean;
 }
 export const ProtectedRoute = ({ children, authenticationRequired }: Props) => {
-  const accesstoken = useAuth((state) => state.accessToken);
+    const accessToken = useAuth((state) => state.accessToken);
+    const navigate = useNavigate();
+    const isAuth = useAuth(state=>state.isAuth);
+    const setIsAuth=useAuth(state=>state.setIsAuth);
+    useEffect(() => {
+      if (authenticationRequired && accessToken === null) {
+        navigate("/login", { replace: true });
+      } else if (!authenticationRequired && accessToken) {
+        navigate("/", { replace: true });
+      } else {
+        setIsAuth(true); // User is authorized
+      }
 
-  if (!authenticationRequired && accesstoken) return <Navigate to="/" />;
-
-  if (authenticationRequired && accesstoken === null)
-    return <Navigate to="/login" />;
-
-  return <>{children}</>;
-};
+      return ()=>{
+        setIsAuth(false);
+      }
+    }, [accessToken, authenticationRequired, navigate]);
+  
+   
+    return !isAuth?<div>Loading ...</div>: <>{children}</>;
+  };
